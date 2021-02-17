f631497f694f8943914bfe7347f90d901d9e175b
DarkThundur <ctubomouj@gmail.com>
2021-02-17 17:49:14 +0600

    added linear correlation
    linear correlation with text on total exercises vs total hours graph
    changed image.

--- Graphing.R
+++ Graphing.R
@@ -1,7 +1,5 @@
 library(tidyverse)
 #Note that for this code to work, clean_data must be intialized and cleaned already and so preprocess_complete_data.R must have been run.
-
-
 #This entire section generates a multitude of visualizations and saves the significant ones
 new_clean_data = clean_data %>% mutate(Exercise_Count = str_count(Exercises, ',') + 1)
 ggplot(data = new_clean_data) + geom_point(mapping = aes(x = Duration, y = Exercise_Count)) #This shows little or no correlation between exercise numbers and duration
@@ -9,7 +7,17 @@ ggplot(data = new_clean_data) + geom_point(mapping = aes(x = Duration, y = Exerc
 # Graphing Information on Instructors (This is the most important section of graphs in my opinion)
 Instructor_Data = new_clean_data %>% group_by(Instructor_ID) %>% summarise(Total_Hours = sum(Duration), Total_Exercises = sum(Exercise_Count), Hourly_Exercises = Total_Exercises/Total_Hours, Average_Hours = (sum(Duration)/(max(Year) - min(Year))))
 ggplot(Instructor_Data) + geom_col(mapping = aes(x = Instructor_ID, y = Total_Hours)) #This plot is Instructors vs total hours worked.
-ggplot(data = Instructor_Data) + geom_point(mapping = aes(x = Total_Hours, y = Total_Exercises)) + ggtitle("Hours of Instruction by Instructors vs Total Exercises Students Completed") + ggsave("img/Exercises_Vs_Duration.png") # Plot total hours vs total exercises. This plot shows that exercises are strongly correlated to time over longer periods of time.
+
+
+#modified by wasim
+Coeftext = paste("r =" , toString(cor(Instructor_Data %>% select(Total_Hours), Instructor_Data %>% select(Total_Exercises), method = c("pearson"))[1][1]))
+ggplot(data = Instructor_Data, aes(x = Total_Hours, y = Total_Exercises)) + geom_point()  +
+  geom_smooth(method='lm', formula= y~x, se = FALSE)+  geom_text(aes(x = 250, y = 300), label = Coeftext) +
+  ggtitle("Hours of Instruction by Instructors vs Total Exercises Students Completed") + ggsave("img/Exercises_Vs_Duration.png") # Plot total hours vs total exercises. This plot shows that exercises are strongly correlated to time over longer periods of time.
+#Wasim: Hey I modified the code above to include a linear regression line and coefficient to show how strong the correlation is
+
+#modified by wasim
+
 ggplot(Instructor_Data) + geom_col(mapping = aes(x = Instructor_ID, y = Hourly_Exercises)) + ggtitle("Average Exercises Students Completed per Hour by Instructor") + ggsave("img/Hourly_Exercises.png") # This plot shows the number of exercises each instructors students completed per hour.
 ggplot(Instructor_Data) + geom_col(mapping = aes(x = Instructor_ID, y = Average_Hours)) + ggtitle("Average Number of Instructing Hours by Instructor") + ggsave("img/Hours_Per_Year.png") # This plot is the average hours of instruction per year for each instructor.
 Instructor_Trendline_Data = Instructor_Data %>% summarize(X_Mean = mean(Average_Hours), Y_Mean = mean(Hourly_Exercises), m = sum((Average_Hours - X_Mean)*(Hourly_Exercises - Y_Mean))/sum((Average_Hours - X_Mean)^2), b = Y_Mean - X_Mean*m)
