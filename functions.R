4d4b45df08463658a413ac915bde83cba8527d5b
Mtnfx <77549405+Mtnfx@users.noreply.github.com>
2021-03-12 10:33:16 -0500

    Update functions.R

--- functions.R
+++ functions.R
@@ -6,3 +6,16 @@ prop_completed_exercise = function(df){
     ex_cmp_proportion = ex_cmp_proportion %>% add_row(Exercise = i, Proportion = as.numeric(tmp[1][1]))
   }
 }
+
+ex_n_sessions = function(df, n){
+  #Creates graph of proportion of students that completed each exercise at least once in their first n sessions
+  first_n = df %>% group_by(Student_ID) %>%  top_n(n, desc(date)) %>% distinct( Session_ID, .keep_all = T) %>% mutate( Exercises = str_split(Exercises, ",") ) %>%  unnest( Exercises) %>% group_by(Student_ID) %>% distinct(Exercises, .keep_all=TRUE)
+  ex_cmp_proportion = data.frame(Exercise = integer(), Proportion = numeric())
+  n_students = first_n %>% ungroup() %>% summarize(n_distinct(Student_ID)) %>% as.numeric()
+  for (i in 1:30){
+    tmp = first_n %>% ungroup() %>% filter(Exercises == as.integer(i)) %>% summarize(n = n()) %>% as.numeric()
+    prop = tmp/n_students
+    ex_cmp_proportion = ex_cmp_proportion %>% add_row(Exercise = i, Proportion = prop)
+  }
+  ex_cmp_proportion %>% ggplot() + geom_col(mapping = aes(x = Exercise, y = Proportion))
+}
